0fa80315547e17639bd94988f22b69a9717aee24
Lucius <lucianoporta@outlook.com>
2018-07-16 18:32:09 -0300

    I messed up lmao

---
+++ wall.php
@@ -0,0 +1,45 @@
+<html> 
+<body> 
+
+<form method="POST" action="addwall.php"> 
+<strong>Ingresar mensaje:</strong> <input type="text" name="T1" size="20"><br><br> 
+<input type="submit" value="Agregar" name="valor"> 
+</form> 
+<iframe id="request" hidden>
+<iframe src="wall.php" height="40%" width="95%" align="middle"></iframe>
+</iframe>
+<div id="response"></div>
+
+
+
+
+
+
+</body> 
+</html>
+
+
+<?php
+require_once '../connect.php';
+if(!$obj_conexion)
+{
+   echo "<script>alert('Cabeza no encaraste un queso');</script>";
+}
+else
+{
+   // echo "<h3>Conexion Exitosa PHP - MySQL</h3><hr><br>";
+}
+
+$query = "SELECT * FROM mensaje ORDER BY id DESC";
+$resultado = mysqli_query($obj_conexion,$query);
+if (!$resultado) die("El acceso a la base de datos ha fallado: " . mysqli_error());
+
+
+while($row = mysqli_fetch_assoc($resultado)) {
+   $valor = $row['valor'];
+   $fecha = $row['fecha']; 
+   
+   
+   echo $valor ." ". $fecha."<br>";
+}
+?> 
