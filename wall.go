faaeb487ca7ddb85f9679f8ac01013cfa5d52c05
Lucius <lucianoporta@outlook.com>
2018-07-16 23:33:18 -0300

    Retrievieng messages from DB implemented

--- wall.go
+++ wall.go
@@ -4,43 +4,73 @@ import (
     "net/http"
     "log"
     "html/template"
-    "database/sql"
     _ "github.com/go-sql-driver/mysql"
+	"database/sql"
 )
 
 type wallPost struct  {
-    Message string
+    Message []string
 }
 
 type pageVariables struct {
 	Title string
-	DBStatus bool
 }
 
 func main() {
-
-    http.HandleFunc("/", DisplayWall)
+    http.HandleFunc("/", DisplayWebsite)
+    http.HandleFunc("/send", SendMessage)
     log.Fatal(http.ListenAndServe(":80", nil))
     //http.HandleFunc("/submit", Submit)
 }
 
-func DisplayWall(w http.ResponseWriter, r *http.Request) {
-    var Wall wallPost
-    var data []string
+func DisplayWebsite(w http.ResponseWriter, r *http.Request) {
 
-    log.Printf("xDDDDD")
+	// ------------------------
+	// Recibimos datos de MySQL
+	// ------------------------
 
-    p := pageVariables{Title: "pene"}
+	var Data []string // Conjunto de variables de tipo wallPost
+	var Wall wallPost // Variable de tipo wallPost
 
-    // Abrimos conexion a MySQL
-    db, err := sql.Open("mysql", "root@tcp(localhost:3306)/thewall")
+	// Abrimos conexion a MySQL
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/thewall")
 
-    p.DBStatus = db.Ping() == nil
+	// Por si hay algun error
+	if err != nil {
+		panic(err.Error())
+	}
 
-    // Por si hay algun error
-    if err != nil {
-        panic(err.Error())
-    }
+	// Recibe datos de MySQL
+	results, err := db.Query("SELECT valueMessages FROM messages")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Recorre los datos de MySQL y los devuelve como una variable data
+	for results.Next() {
+		var message string
+
+		err = results.Scan(&message)
+
+
+		Data = append(Data, message)
+		//log.Printf(Wall.Message)
+
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	Wall.Message = Data
+
+	// Cuando termine la funcion Main, cierra la conexion
+	defer db.Close()
+
+	// ------------------------------------
+	// Usamos el template para enviar datos
+	// ------------------------------------
+
+	//p := pageVariables{Title: "pene"}
 
     t, err := template.ParseFiles("select.html")
 
@@ -48,33 +78,13 @@ func DisplayWall(w http.ResponseWriter, r *http.Request) {
         log.Print("Hubo un eror mostrando esta pagina: ", err)
     }
 
-    results, err := db.Query("SELECT valueMessages FROM messages")
+    err = t.Execute(w, Wall)
 
-    if err != nil {
-        panic(err.Error())
-    }
-
-    for results.Next() {
-        var message string
-
-        log.Print("xD")
-
-        err = results.Scan(&message)
-
-        Wall.Message = message
-        data = append(data, Wall.Message)
-        log.Printf(Wall.Message)
-
-        if err != nil {
-            panic(err.Error())
-        }
-    }
-
-    err = t.Execute(w, p)
     if err != nil {
         log.Printf("Problema al enviar a HTML ", err)
     }
+}
 
-    // Cuando termine la funcion Main, cierra la conexion
-    defer db.Close()
+func SendMessage(w http.ResponseWriter, r *http.Request) {
+
 }
