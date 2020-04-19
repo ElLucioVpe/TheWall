9fcd91b6800ac26a4e554c0e046efc4941d2892b
Lucius <lucianoporta@outlook.com>
2020-04-19 16:35:46 -0300

    Added image preview on button

--- src/TheWall/js/custom.js
+++ src/TheWall/js/custom.js
@@ -1,11 +1,19 @@
-console.log("Estas re puto");
-
-$( document ).ready(function() {
-    var dateSQL = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
-    var txtDate = document.getElementById("txtDate");
-
-    console.log(dateSQL);
-
-    txtDate.value = dateSQL;
+$(document).ready(function () {
+  $("#imgInp").change(function () {
+    readURL(this);
+  });
 });
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $("#imgUpload").css("background-image", "url(" + e.target.result + ")");
+      console.log("aa");
+      $("#imgUpload").css("background-size", "cover");
+    };
+
+    reader.readAsDataURL(input.files[0]); // convert to base64 string
+  }
+}
